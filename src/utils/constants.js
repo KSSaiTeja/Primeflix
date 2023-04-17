bff3fe16d86f27f7fdd1d1fe91f393e15db032a6
KSSaiTeja <saitej4865@gmail.com>
2023-04-17 09:36:58 +0530

    Final upload in the project

---
+++ src/utils/constants.js
@@ -0,0 +1,51 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import TagSharpIcon from '@mui/icons-material/TagSharp';
+import MovieIcon from '@mui/icons-material/Movie';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import WcIcon from '@mui/icons-material/Wc';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+
+
+export const logo = 'https://i.ibb.co/ZdT2CMJ/Primeflixx.png'
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'Trending', icon: <TagSharpIcon />, },
+  { name: 'Telugu Movie', icon: <MovieIcon />, },
+  { name: 'Telugu Comedy', icon: <EmojiEmotionsIcon />, },
+  { name: 'Telugu Rom-Com', icon: <WcIcon />, },
+  { name: 'Telugu Songs', icon: <LibraryMusicIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'ReactJS', icon: <TagSharpIcon/>, },
+  { name: 'NextJS', icon: <TagSharpIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'KSSAITEJ&KARTHIKEYA';
+export const demoVideoTitle = 'Youtube_2.0';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
